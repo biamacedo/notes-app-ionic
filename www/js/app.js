ff296bcd1918154eaea38833468b1056845d3a42
biamacedo <bia.macedo13@gmail.com>
2016-01-02 20:38:28 -0200

    Finished tutorial

--- www/js/app.js
+++ www/js/app.js
@@ -1,44 +1,85 @@
-var app = angular.module('starter', ['ionic']);
+(function(){
+    var app = angular.module('mynotes', ['ionic', 'mynotes.notestore']);
 
-app.controller('ListCtrl', function($scope){
+    app.config(function($stateProvider, $urlRouterProvider){
 
-    $scope.notes = [
-        {
-            title: 'First Note',
-            description: 'This is my first note'
-        },
-        {
-            title: 'Second Note',
-            description: 'This is my second note'
-        }
+        $stateProvider.state('list', {
+            url: '/list',
+            templateUrl: 'templates/list.html'
+        });
 
-    ];
+        $stateProvider.state('edit', {
+            url: '/edit/:noteId',
+            templateUrl: 'templates/edit.html',
+            controller: 'EditCtrl'
+        });
 
-});
+        $stateProvider.state('add', {
+            url: '/add',
+            templateUrl: 'templates/edit.html',
+            controller: 'AddCtrl'
+        });
 
-app.config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/list');
 
-    $stateProvider.state('list', {
-        url: '/list',
-        templateUrl: 'templates/list.html'
-    });
-    $stateProvider.state('edit', {
-        url: '/edit',
-        templateUrl: 'templates/edit.html'
     });
 
-    $urlRouterProvider.otherwise('/list');
+    app.controller('ListCtrl', function($scope, NoteStore){
 
-});
+        $scope.notes = NoteStore.list();
+        $scope.reordering = false;
 
-app.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+        $scope.remove = function(noteId){
+            NoteStore.remove(noteId);
         }
-        if(window.StatusBar) {
-            StatusBar.styleDefault();
+
+        $scope.move = function(note, fromIndex, toIndex){
+            NoteStore.move(note, fromIndex, toIndex);
         }
+
+        $scope.toggleReordering = function(){
+            $scope.reordering = !$scope.reordering;
+        }
+
     });
-});
+
+    app.controller('EditCtrl', function($scope, $state, NoteStore){
+
+        $scope.note = angular.copy(NoteStore.get($state.params.noteId));
+
+        $scope.save = function(){
+            NoteStore.update($scope.note);
+            $state.go('list');
+        }
+
+    });
+
+    app.controller('AddCtrl', function($scope, $state, NoteStore){
+
+        $scope.note = {
+            id: new Date().getTime().toString(),
+            title: '',
+            description: ''
+        };
+
+        $scope.save = function(){
+            NoteStore.create($scope.note);
+            $state.go('list');
+        }
+
+    });
+
+
+    app.run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    });
+
+}());
